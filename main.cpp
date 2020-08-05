ba8029514353bca41917c245d3fa678931c98bca
SketchyLxve <twitchisadeck@gmail.com>
2020-08-06 01:28:52 +0200

    adding small details; nothing important

--- main.cpp
+++ main.cpp
@@ -5,15 +5,18 @@
 #include <stdio.h>
 #include <vector>
 #include <string>
+#include <array>
 
 using namespace std;
 
 int main()
 {
-  string input = "your mom --gay=true --additional=\"very fucking dumb\"";
-  vector<ParsedFlag> flags = parse_flags(input);
+    string input = "your mom --gay=true --additional=\"very fucking dumb\"";
+    vector<ParsedFlag> flags = parse_flags(input);
 
-  for (int i = 0; i < flags.size(); i++)
-    cout << "Initial: "  << flags[i].initial << "\nFlag: " << flags[i].flag << "\nValue: " << flags[i].value << endl;
-  return 0;
+    cout << "Input: " << input << "\n";
+
+    for (int i = 0; i < flags.size(); i++)
+        cout << "\n  Iteration " << i + 1 << "  |" << "\n================\n" << "Initial: "  << flags[i].initial << "\nFlag: " << flags[i].flag << "\nValue: " << flags[i].value << endl;
+    return 0;
 }
